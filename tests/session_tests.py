1945d4bf5229e5374e1d46793473e9988c075295
Vincent Wong <duperduper@ucla.edu>
2017-02-20 12:07:55 -0800

    Fix session tests to not fail on no credentials
    It will skip the login test instead, and nosetest will indicate it as
    skipped.

--- tests/session_tests.py
+++ tests/session_tests.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from nose.tools import *
+from unittest import SkipTest
+from functools import wraps
 import myanimelist.session
 import myanimelist.anime
 import os
+
+import sys
 
 class testSessionClass(object):
   @classmethod
@@ -15,22 +19,35 @@ class testSessionClass(object):
       self.password = os.environ[u'MAL_PASSWORD']
     else:
       # rely on a flat textfile in project root.
-      with open(u'credentials.txt', 'r') as cred_file:
-        line = cred_file.read().strip().split(u'\n')[0]
+      try:
+        with open(u'credentials.txt', 'r') as cred_file:
+          line = cred_file.read().strip().split(u'\n')[0]
         self.username, self.password = line.strip().split(u',')
+      except IOError:
+        self.username = self.password = None
 
     self.session = myanimelist.session.Session(self.username, self.password)
-    self.logged_in_session = myanimelist.session.Session(self.username, self.password).login()
     self.fake_session = myanimelist.session.Session(u'no-username', 'no-password')
-  def testLoggedIn(self):
+
+  def skipIfNoCredentials(inner):
+    @wraps(inner)
+    def wrapper(self, *args, **kwargs):
+      if not self.username:
+        raise SkipTest('no credentials provided for testing')
+      return inner(self, *args, **kwargs)
+    return wrapper
+
+  def testFailFakeLogin(self):
     assert not self.fake_session.logged_in()
     self.fake_session.login()
     assert not self.fake_session.logged_in()
     assert not self.session.logged_in()
-    assert self.logged_in_session.logged_in()
+
+  def testAnime(self):
+    assert isinstance(self.session.anime(1), myanimelist.anime.Anime)
+
+  @skipIfNoCredentials
   def testLogin(self):
     assert not self.session.logged_in()
     self.session.login()
     assert self.session.logged_in()
-  def testAnime(self):
-    assert isinstance(self.session.anime(1), myanimelist.anime.Anime)
